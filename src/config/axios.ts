108c4ae3420716a2e3a887049d7632004e5e48e5
Dev 05 <Dev 05@DESKTOP-SC66BM9>
2025-12-07 17:28:29 +0330

    fix proxy

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
+
+const API_BASE = import.meta.env.DEV
+  ? '/api'
+  : 'https://pro-api.coinmarketcap.com/v1'
+
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: API_BASE,
   headers: {
     'X-CMC_PRO_API_KEY': '8402ff41-c276-40ba-898b-bf3577908c61',
   },
